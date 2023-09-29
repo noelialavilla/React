75aaaa6e0d4616aeb3770bcf23bdc2e36604eb27
Noelia Lavilla <noelia.m.lavilla@gmail.com>
2023-09-29 00:48:51 -0300

    Se aplica responsive

--- mi-tienda/src/App.jsx
+++ mi-tienda/src/App.jsx
@@ -6,19 +6,23 @@ import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
+
     <header className="container-fluid">
       <div className="row">
         <a className="navbar-brand" href="./index.html">
-        <img src={logo} className="logo" alt="GALogo" title="Grace Aromas" />
+          <img src={logo} className="logo" alt="GALogo" title="Grace Aromas" />
         </a>
       </div>
       <div className="row col-12">
 
         <NavBar />
-        <ItemListContainer greeting= "Bienvenido a Grace Aromas"/>
+        <ItemListContainer greeting="Bienvenido a Grace Aromas" />
 
       </div>
     </header>
+    
+    
+
   );
 }
 
